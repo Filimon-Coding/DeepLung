65451e9e0b3a1910525a38cb016e7c6d3983f8b6
Mina2334 <mimah2000@oslomet.no>
2026-02-10 22:05:06 +0100

    Add frontend (Vite + React + TypeScript)

---
+++ src/pages/AnalyzePage.tsx
@@ -0,0 +1,12 @@
+function AnalyzePage() {
+  return (
+    <div>
+      <h1>Image Analysis</h1>
+      <p>
+        This page will display the analysis results of the selected image.
+      </p>
+    </div>
+  );
+}
+
+export default AnalyzePage;
