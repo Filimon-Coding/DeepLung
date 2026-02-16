416be95bd98647e6c0db9f437313b0b3df4348fd
Mina2334 <mimah2000@oslomet.no>
2026-02-17 00:36:13 +0100

    Add multi-page routing with Navbar and Results page

---
+++ frontend/src/pages/ResultsPage.tsx
@@ -0,0 +1,12 @@
+function ResultsPage() {
+  return (
+    <div>
+      <h1>Image Analysis Results</h1>
+      <p>
+        This page will display the analysis results of the selected image.
+      </p>
+    </div>
+  );
+}
+
+export default ResultsPage;
