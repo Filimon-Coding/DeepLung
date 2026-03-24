4146b2f2ea46617f47d13aaeb0a9471699e93c30
Filimon-Coding <filimon.nuguse22@gmail.com>
2026-03-24 11:19:27 +0100

    Sync code changes: rename files, remove protype (exclude LFS datasets)

--- underDev3/protype/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="w-full bg-[#1f3b63] text-white">
-      <div className="max-w-6xl mx-auto px-6 py-5 text-center text-sm">
-        Copyright © 2026 CRAI. All rights reserved.
-      </div>
-    </footer>
-  );
-}
