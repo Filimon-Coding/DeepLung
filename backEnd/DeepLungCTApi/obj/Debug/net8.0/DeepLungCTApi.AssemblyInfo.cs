8b9d5727cee2f2a5141171091dde009ee146a02c
Filimon-Coding <filimon.nuguse22@gmail.com>
2026-03-29 19:48:26 +0200

    updating before the pipeline

--- backEnd/DeepLungCTApi/obj/Debug/net8.0/DeepLungCTApi.AssemblyInfo.cs
+++ backEnd/DeepLungCTApi/obj/Debug/net8.0/DeepLungCTApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeepLungCTApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0484220ab35a894bc8a7d4d8f286407c7a49e6c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d73c60be787dc17bbf130a95b3a040968ebd3461")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeepLungCTApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeepLungCTApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
