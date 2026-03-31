4ffe7025fd882143b9150577491b1db60f4977fd
Filimon-Coding <filimon.nuguse22@gmail.com>
2026-03-31 13:42:57 +0200

    Lungvisual component for the hero sec

---
+++ frontEnd/src/components/LungVisual.tsx
@@ -0,0 +1,13 @@
+import lungsImg from "../assets/lungs-3d.png";
+
+function LungVisual() {
+  return (
+    <img
+      src={lungsImg}
+      alt="3D lung illustration"
+      className="lung-img"
+    />
+  );
+}
+
+export default LungVisual;
