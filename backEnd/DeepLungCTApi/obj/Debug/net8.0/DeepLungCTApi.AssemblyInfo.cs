1e4b75cee8e9f74e948299fbd37547eb567447ea
Filimon-Coding <filimon.nuguse22@gmail.com>
2026-04-16 13:49:08 +0200

    new artifact from the newly trained model, it include the statisic and apporach of the whole process aswell, gone include on the report

--- backEnd/DeepLungCTApi/obj/Debug/net8.0/DeepLungCTApi.AssemblyInfo.cs
+++ backEnd/DeepLungCTApi/obj/Debug/net8.0/DeepLungCTApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeepLungCTApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+730442d75b9cf892d6ba51116f063e27de2471d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f593dd15e578357b0608238b525f5bc11c3ccf")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeepLungCTApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeepLungCTApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
