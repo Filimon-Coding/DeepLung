87e4b40501caacb50d3b825bc71193dabd56fb67
Filimon-Coding <filimon.nuguse22@gmail.com>
2026-04-27 13:03:39 +0200

    New Group just made and new name space

--- backEnd/DeepLungCTApi/obj/Debug/net8.0/DeepLungCTApi.AssemblyInfo.cs
+++ backEnd/DeepLungCTApi/obj/Debug/net8.0/DeepLungCTApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeepLungCTApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d382f304c3ed8d79769f1b9e9e75272d4f5ddfca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed211f01fa1173d26f0449f2cba5a86e96ef8800")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeepLungCTApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeepLungCTApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
